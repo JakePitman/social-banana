3c7c50e9d939e1819fd8e1d4f4d60c71ea0df0cf
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-16 19:26:52 +1000

    add logic to communicate to backend via web-dev-server proxy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+// This is to test the proxy of the front end server for development
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
