68173c7b2e160dd9fefb540394ad3bdf8031eb52
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-16 23:35:40 +1000

    update path.join for index.html

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.get('/api/hello', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
