9ba86df17dd835cb6944a7a7033029e5ee330392
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-16 22:29:42 +1000

    add configuration for deployment on heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // This is to test the proxy of the front end server for development
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
