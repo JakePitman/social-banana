5c90cf715451ca415132312f5c1e2720bed17056
Mark Tice <marktice@protonmail.com>
2018-07-17 14:17:12 +1000

    Add mongoose and connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+
 // This is to test the proxy of the front end server for development
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+// TODO: User Routes
+// TODO: setup mongoose
 
 // After building the front end to static files, get express to host it in production
 if (process.env.NODE_ENV === 'production') {
