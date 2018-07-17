53c839e1e863416ab57a1eb88f7cd01cb7a6c071
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-17 15:19:47 +1000

    delete normalize.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
 import './css/components/loginForm.css';
 import App from './js/components/App';
 
