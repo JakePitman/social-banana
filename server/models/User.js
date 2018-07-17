3bf5fce1e88d6e5f564ac950fe2d71e6895c2c63
Mark Tice <marktice@protonmail.com>
2018-07-17 16:26:20 +1000

    Add bcrypt of password on user pre save

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -16,6 +17,21 @@ const UserSchema = new mongoose.Schema({
   linkedIn: {
     toggleStatus: Boolean,
     access_token: String
+  }
+});
+
+// Pre save, hash password
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
   }
 });
 
