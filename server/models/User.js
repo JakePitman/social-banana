66b08e5ecea0765c51e2ae04d8a1f170315c2c91
Mark Tice <marktice@protonmail.com>
2018-07-17 16:30:04 +1000

    Add jwt and User instance and static methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
@@ -19,6 +20,64 @@ const UserSchema = new mongoose.Schema({
     access_token: String
   }
 });
+
+// Instance methods (user)
+UserSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  try {
+    const token = jwt
+      .sign({ _id: user._id }, process.env.JWT_SECRET)
+      .toString();
+    user.authTokens.push(token);
+    await user.save();
+    return token;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+UserSchema.methods.removeToken = async function(token) {
+  const user = this;
+  try {
+    return user.update({ $pull: { authTokens: token } });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+// Static methods (User)
+UserSchema.statics.findByToken = async function(token) {
+  const User = this;
+  try {
+    const decodedId = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decodedId, authTokens: token });
+    if (!user) {
+      throw new Error('No user with given token');
+    }
+    return user;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+UserSchema.statics.findByCredentials = async function(email, password) {
+  const User = this;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error('No user by that email');
+    }
+    const result = await bcrypt.compare(password, user.password);
+    if (result) {
+      return user;
+    } else {
+      throw new Error('Incorrect password');
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
 // Pre save, hash password
 UserSchema.pre('save', function(next) {
