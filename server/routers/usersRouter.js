35ce24ae9b5f3f7fa0cb6db9446c98f00e38c8a9
Mark Tice <marktice@protonmail.com>
2018-07-17 16:19:17 +1000

    Move users routes to userRouter

---
+++ server/routers/usersRouter.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+const { User } = require('./../models/User');
+
+usersRouter.get('/me', async (req, res) => {
+  try {
+    console.log('hello from /api/users/me');
+    const user = await User.findOne({ email: 'MargaretBananaDev@gmail.com' });
+    if (!user) {
+      throw new Error();
+    }
+    res.status(200).send({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ error: error.message });
+  }
+});
+
+usersRouter.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    if (!user) {
+      throw new Error();
+    }
+    res.status(200).send({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ error: error.message });
+  }
+});
+
+module.exports = { usersRouter };
