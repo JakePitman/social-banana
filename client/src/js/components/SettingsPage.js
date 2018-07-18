0fe5c6f1db0a7de68914973d586e73c3d02510da
Barbara Rodriguez <barbs2089@gmail.com>
2018-07-18 16:48:15 +1000

    fixed merge issue

--- client/src/js/components/SettingsPage.js
+++ client/src/js/components/SettingsPage.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import placeholder from '../../css/img/placeholder.png';
-=======
 import React from 'react'
 import placeholder from '../../css/img/placeholder.png'
 import loginlinkedin from '../../css/img/sign-in/Sign-In-Small---Default.png'
 
 
->>>>>>> Add linkedin button
 
 class SettingsPage extends React.Component {
   constructor(props) {
@@ -37,7 +32,6 @@ class SettingsPage extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <div className="profile">
         <form className="profile-form" onSubmit={this.handleSubmit}>
           <div className="profile-section">
@@ -46,7 +40,6 @@ class SettingsPage extends React.Component {
               src={placeholder}
               width="200"
               height="200"
-              alt="logo"
               alt="profile-picture"
               id="imageID"
             />
@@ -88,53 +81,5 @@ class SettingsPage extends React.Component {
       </div>
     );
   }
-=======
-        <div className="profile">
-            <form className="profile-form" onSubmit={this.handleSubmit}>
-                    <div className='profile-section'>
-                        <img
-                        className="profile-picture"
-                        src={placeholder} width="200" height="200" alt="logo"
-                        alt="profile-picture"
-                        id="imageID"
-                        />
-                      <label htmlFor='name'>
-                      Name   
-                      </label>
-                      <input type='text' value={this.state.name} onChange={this.handleChange} name='name' />
-                      <label htmlFor='company'>
-                      Company
-                      </label>
-                      <input type='text' value={this.state.company} onChange={this.handleChange} name='company' />
-                    </div>
-                 <form className="profile-form-input">
-                    <label htmlFor='phone'>
-                    Phone
-                    </label>
-                    <input type='text' value={this.state.phone} onChange={this.handleChange} name='phone' />
-                    <label htmlFor='email'>
-                      Email
-                    </label>
-                    <input type="text" value={this.state.email} onChange={this.handleChange} name='email' />
-                    </form>
-                <div className="social-login">
-                <script type="in/Login">
-                    Hello, <?js= firstName ?> <?js= lastName ?>.
-                      </script>
-                     <img
-                        src={loginlinkedin}
-                        alt="loginlinkedin"
-                        id="imageID"
-                        />
-                  </div>
-                <button className='cta-primary' type='submit'>
-                Update
-                </button>
-            </form>
-        </div>
-        )
-    }
-    
->>>>>>> Add linkedin button
 }
 export { SettingsPage };
