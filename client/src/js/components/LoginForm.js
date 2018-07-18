f6a900adf8b6c870c64c60976cc25321968f1e53
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-18 20:49:04 +1000

    clean loginform syntax

--- client/src/js/components/LoginForm.js
+++ client/src/js/components/LoginForm.js
@@ -1,63 +1,62 @@
-import React from 'react'
+import React from 'react';
 import logo from '../../css/img/logo.png';
 
 class LoginForm extends React.Component {
+  state = {
+    formType: 'login',
+  };
 
-    state = {
-        formTypeIsLogin: true
-    };
+  render() {
+    return (
+      <form
+        className={
+          this.state.formType === 'login'
+            ? 'form form-loginType'
+            : 'form form-registerType'
+        }
+      >
+        <div id="logoWorks">
+          <img src={logo} width="200" height="200" alt="logo" />
+        </div>
 
-    render() {
+        <div className="form-toggle">
+          <button
+            className="login-toggle"
+            onClick={e => {
+              e.preventDefault();
+              this.setState({
+                formType: 'login',
+              });
+            }}
+          >
+            Login
+          </button>
 
-        return (
-            <form className={this.state.formTypeIsLogin ? 'form form-loginType' : 'form form-registerType'}>
+          <button
+            className="register-toggle"
+            onClick={e => {
+              e.preventDefault();
+              this.setState({
+                formType: 'register',
+              });
+            }}
+          >
+            Register
+          </button>
+        </div>
 
-                <div id="logoWorks">
-                    <img src={logo} width="200" height="200" alt="logo" />
-                </div>
-
-                <div className="form-toggle">
-                    <a className="login-toggle active"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            this.setState({
-                                formTypeIsLogin: true
-                            });
-                        }}
-                    >
-                        <p className={this.state.formTypeIsLogin ? "active" : ''}>
-                            Login
-                        </p>
-                    </a>
-
-                    <a className="register-toggle"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            this.setState({
-                                formTypeIsLogin: false
-                            });
-                        }}
-                    >
-                        <p className={this.state.formTypeIsLogin ? "" : "active"}>
-                            Register
-                        </p>
-                    </a>
-                </div>
-
-                <div className="form-content">
-                    <div className="form-input">
-                        <input type="text" placeholder="email" />
-                        <input type="text" placeholder="Password" />
-                    </div>
-                </div>
-
-                <button type="submit" >
-                    {this.state.formTypeIsLogin ? 'Login' : 'Register'}
-                </button>
-
-            </form >
-        );
-    }
+        <div className="form-content">
+          <div className="form-input">
+            <input type="text" placeholder="email" />
+            <input type="text" placeholder="Password" />
+          </div>
+          <button type="submit">
+            {this.state.formType === 'login' ? 'Login' : 'Register'}
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
-export { LoginForm }
+export default LoginForm;
