0f86b47007e9339dbd866494bc59c172d4b4deff
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-18 16:23:31 +1000

    fix deplicate in settingspage.js

--- client/src/js/components/SettingsPage.js
+++ client/src/js/components/SettingsPage.js
@@ -1,73 +1,83 @@
-import React from 'react'
-import placeholder from '../../css/img/placeholder.png'
-
+import React from 'react';
+import placeholder from '../../css/img/placeholder.png';
 
 class SettingsPage extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { 
-        picture: this.props.picture,
-        name: this.props.name,
-        company: this.props.company,
-        phone: this.props.phone,
-        email: this.props.email,
-        isOpen: false,
-      
-      }
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      picture: this.props.picture,
+      name: this.props.name,
+      company: this.props.company,
+      phone: this.props.phone,
+      email: this.props.email,
+      isOpen: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange (event) {
-    event.preventDefault()
+  handleChange(event) {
+    event.preventDefault();
     this.setState({
-      [event.target.name]: event.target.value
-    })
+      [event.target.name]: event.target.value,
+    });
   }
 
-  handleSubmit (event) {
+  handleSubmit(event) {
     alert('Updated successfully!');
-    event.preventDefault()
+    event.preventDefault();
   }
-
 
   render() {
     return (
-        <div className="profile">
-            <form className="profile-form" onSubmit={this.handleSubmit}>
-                    <div className='profile-section'>
-                        <img
-                        className="profile-picture"
-                        src={placeholder} width="200" height="200" alt="logo"
-                        alt="profile-picture"
-                        id="imageID"
-                        />
-                      <label htmlFor='name'>
-                      Name   
-                      </label>
-                      <input type='text' value={this.state.name} onChange={this.handleChange} name='name' />
-                      <label htmlFor='company'>
-                      Company
-                      </label>
-                      <input type='text' value={this.state.company} onChange={this.handleChange} name='company' />
-                    </div>
-                 <form className="profile-form-input">
-                    <label htmlFor='phone'>
-                    Phone
-                    </label>
-                    <input type='text' value={this.state.phone} onChange={this.handleChange} name='phone' />
-                    <label htmlFor='email'>
-                      Email
-                    </label>
-                    <input type="text" value={this.state.email} onChange={this.handleChange} name='email' />
-                    </form>
-                <button className='cta-primary' type='submit'>
-                Update
-                </button>
-            </form>
-        </div>
-        )
-    }
-    
+      <div className="profile">
+        <form className="profile-form" onSubmit={this.handleSubmit}>
+          <div className="profile-section">
+            <img
+              className="profile-picture"
+              src={placeholder}
+              width="200"
+              height="200"
+              alt="logo"
+              alt="profile-picture"
+              id="imageID"
+            />
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              name="name"
+            />
+            <label htmlFor="company">Company</label>
+            <input
+              type="text"
+              value={this.state.company}
+              onChange={this.handleChange}
+              name="company"
+            />
+          </div>
+          <form className="profile-form-input">
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="text"
+              value={this.state.phone}
+              onChange={this.handleChange}
+              name="phone"
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.handleChange}
+              name="email"
+            />
+          </form>
+          <button className="cta-primary" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-export { SettingsPage }
+export { SettingsPage };
