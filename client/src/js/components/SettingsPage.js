ad30297372faff050243e1524b5b3b51e7d1e3a9
Barbara Rodriguez <barbs2089@gmail.com>
2018-07-18 14:25:26 +1000

    Add linkedin button

--- client/src/js/components/SettingsPage.js
+++ client/src/js/components/SettingsPage.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import React from 'react';
 import placeholder from '../../css/img/placeholder.png';
+=======
+import React from 'react'
+import placeholder from '../../css/img/placeholder.png'
+import loginlinkedin from '../../css/img/sign-in/Sign-In-Small---Default.png'
+
+
+>>>>>>> Add linkedin button
 
 class SettingsPage extends React.Component {
   constructor(props) {
@@ -29,6 +37,7 @@ class SettingsPage extends React.Component {
 
   render() {
     return (
+<<<<<<< HEAD
       <div className="profile">
         <form className="profile-form" onSubmit={this.handleSubmit}>
           <div className="profile-section">
@@ -79,5 +88,50 @@ class SettingsPage extends React.Component {
       </div>
     );
   }
+=======
+        <div className="profile">
+            <form className="profile-form" onSubmit={this.handleSubmit}>
+                    <div className='profile-section'>
+                        <img
+                        className="profile-picture"
+                        src={placeholder} width="200" height="200" alt="logo"
+                        alt="profile-picture"
+                        id="imageID"
+                        />
+                      <label htmlFor='name'>
+                      Name   
+                      </label>
+                      <input type='text' value={this.state.name} onChange={this.handleChange} name='name' />
+                      <label htmlFor='company'>
+                      Company
+                      </label>
+                      <input type='text' value={this.state.company} onChange={this.handleChange} name='company' />
+                    </div>
+                 <form className="profile-form-input">
+                    <label htmlFor='phone'>
+                    Phone
+                    </label>
+                    <input type='text' value={this.state.phone} onChange={this.handleChange} name='phone' />
+                    <label htmlFor='email'>
+                      Email
+                    </label>
+                    <input type="text" value={this.state.email} onChange={this.handleChange} name='email' />
+                    </form>
+                <div className="social-login">
+                     <img
+                        src={loginlinkedin} width="150"
+                        alt="loginlinkedin"
+                        id="imageID"
+                        />
+                  </div>
+                <button className='cta-primary' type='submit'>
+                Update
+                </button>
+            </form>
+        </div>
+        )
+    }
+    
+>>>>>>> Add linkedin button
 }
 export { SettingsPage };
