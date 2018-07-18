088aec126c158028981430b7e603a79776e06bdc
Jay Pitman <jaypitman@Jays-MacBook-Pro.local>
2018-07-18 14:26:31 +1000

    change LinkedIn.png to linkedIn.png

--- client/src/js/components/MediaBox.js
+++ client/src/js/components/MediaBox.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function MediaBox(props) {
+  const logo = require('../../css/img/' + props.media + '.png')
   return (
     <div className="listing-media-box">
-        <img className="listing-media-logo" src={require(`../../css/img/${props.media}.png`)} alt="linkedin_logo" />
+        <img className="listing-media-logo" src={logo} alt="linkedin_logo" />
         <div className="listing-media-title-container">
           <h3 className="listing-media-title">{props.media}</h3>
         </div>
