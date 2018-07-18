5803edfb137747f0af44858c6edc0f26b6c4c8e6
Jay Pitman <jaypitman@Jays-MacBook-Pro.local>
2018-07-18 15:03:49 +1000

    changed require to import

--- client/src/js/components/MediaBox.js
+++ client/src/js/components/MediaBox.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import linkedInLogo from '../../css/img/linkedIn.png'
 
 function MediaBox(props) {
-  const logo = require('../../css/img/' + props.media + '.png')
   return (
     <div className="listing-media-box">
-        <img className="listing-media-logo" src={logo} alt="linkedin_logo" />
+        <img className="listing-media-logo" src={linkedInLogo} alt="linkedin_logo" />
         <div className="listing-media-title-container">
           <h3 className="listing-media-title">{props.media}</h3>
         </div>
