01e8a8c780219791a150f357cb8d62c152ba60ec
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-19 12:21:43 +1000

    add navBar to apply flexbox

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -44,16 +44,18 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <p>
-            <Link to="/">Login</Link>
-          </p>
-          <p>
-            <Link to="/settings">Settings</Link>
-          </p>
-          <p>
-            <Link to="/listing">Listing</Link>
-          </p>
-
+          <div className="navBar">
+            <div>
+              <Link to="/" style={{ textDecoration: 'none', color: '#908F8F' }}>Login</Link>
+            </div>
+            <div>
+              <Link to="/settings" style={{ textDecoration: 'none', color: '#908F8F' }}>Settings</Link>
+            </div>
+            <div>
+              <Link to="/listing" style={{ textDecoration: 'none', color: '#908F8F' }}>Listing</Link>
+            </div>
+          </div>
+          
           <Switch>
             <Route exact path="/" component={LoginForm} />
             <Route path="/settings" component={SettingsPage} />
