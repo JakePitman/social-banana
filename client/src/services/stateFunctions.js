4a264f82516c807757865673731e82e95fc359b9
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-20 21:34:38 +1000

    clean all

--- client/src/services/stateFunctions.js
+++
@@ -1,8 +0,0 @@
-export function handleToggle(e) {
-  const target = e.target.id;
-  if (target === 'linkedInToggleButton') {
-    this.setState({linkedInToggleStatus: !this.state.linkedInToggleStatus});
-  } else if (target === 'facebookToggleButton') {
-    //FB TOGGLE CODE CAN BE ADDED HERE
-  }
-}
