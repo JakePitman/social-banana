085ff65a6ba829e647abddf5e3aa824ffbd22147
Barbara Rodriguez <barbs2089@gmail.com>
2018-07-24 12:22:44 +1000

    Cleaned profile page and settings page

--- client/src/Settings/Profile.js
+++ client/src/Settings/Profile.js
@@ -19,7 +19,7 @@ class Profile extends React.Component {
   }
 
   handleSubmit(e) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('Successfully Updated ' + this.state.value);
     e.preventDefault();
   }
 
