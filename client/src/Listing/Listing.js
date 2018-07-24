13b3504984b91dd0e79c081f1a787c094744b86f
Jay Pitman <jaypitman@Jays-MacBook-Pro.local>
2018-07-24 16:13:37 +1000

    convert Listing component to stateful

--- client/src/Listing/Listing.js
+++ client/src/Listing/Listing.js
@@ -6,148 +6,206 @@ import './listing.css';
 import socialAPI from '../services/socialAPI';
 import { Redirect } from 'react-router-dom';
 
-function ListingsPage(props) {
-  const toggleSettings = {
-    linkedIn: props.stateCopy.linkedInToggleStatus,
-    twitter: props.stateCopy.twitterToggleStatus
+class ListingsPage extends React.Component {
+  redirectHome = this.props.stateCopy.redirectHome;
+
+  state = {
+    propertyType: 'house',
+    address: '',
+    description: '',
+    price: '',
+    landSize: '',
+    inspectionDate: '',
+    inspectionTime: '',
+    bedrooms: '0',
+    bathrooms: '0',
+    carSpaces: '0'
   };
 
-  const redirectHome = props.stateCopy.redirectHome;
+  handleChange = (e) => {
+    const name = e.target.name;
+    this.setState({ [name]: e.target.value });
+  };
 
-  const handleSubmit = async (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = {};
-    const children = e.target.children;
-    for (let i = 0; i < children.length; i++) {
-      formData[`${children[i].name}`] = children[i].value;
-    }
-    //to get inspectiontime values (nested in div)
-    const inspectionTimeValues = children[12].children;
-    for (let i = 0; i < inspectionTimeValues.length; i++) {
-      formData[`${inspectionTimeValues[i].name}`] =
-        inspectionTimeValues[i].value;
-    }
-    //to get amenities values (nested in divs within a div)
-    const amenitiesValues = children[14].children;
-    for (let i = 0; i < amenitiesValues.length; i++) {
-      formData[`${amenitiesValues[i].children[1].name}`] =
-        amenitiesValues[i].children[1].value;
-    }
-    console.log(formData);
+    //const formData = {};
+    //const children = e.target.children;
+    //for (let i = 0; i < children.length; i++) {
+    //formData[`${children[i].name}`] = children[i].value;
+    //}
+    ////to get inspectiontime values (nested in div)
+    //const inspectionTimeValues = children[12].children;
+    //for (let i = 0; i < inspectionTimeValues.length; i++) {
+    //formData[`${inspectionTimeValues[i].name}`] =
+    //inspectionTimeValues[i].value;
+    //}
+    ////to get amenities values (nested in divs within a div)
+    //const amenitiesValues = children[14].children;
+    //for (let i = 0; i < amenitiesValues.length; i++) {
+    //formData[`${amenitiesValues[i].children[1].name}`] =
+    //amenitiesValues[i].children[1].value;
+    //}
+    //console.log(formData);
 
+    const toggleSettings = {
+      linkedIn: this.props.stateCopy.linkedInToggleStatus,
+      twitter: this.props.stateCopy.twitterToggleStatus
+    };
+    console.log(this.state);
     const res = await socialAPI.shareListing(
-      formData,
-      props.stateCopy.authToken,
+      this.state,
+      this.props.stateCopy.authToken,
       toggleSettings
     );
     console.log(res.linkedInUrl);
     console.log(res.twitterUrl);
 
     //sets redirectHome to true in App.js
-    props.useRedirectHome();
+    this.props.useRedirectHome();
   };
 
-  if (props.stateCopy.redirectHome) {
-    //reset redirectHome to false in App.js before redirecting to home
-    //props.resetRedirectHome();
-    return <Redirect to="/" />;
-  }
+  render() {
+    if (this.props.stateCopy.redirectHome) {
+      //reset redirectHome to false in App.js before redirecting to home
+      //props.resetRedirectHome();
+      return <Redirect to="/" />;
+    }
 
-  return (
-    <div>
-      {/*---------------------FORM--------------------------*/}
-      <form onSubmit={handleSubmit} className="listing-media-form">
-        <h1 className="listing-title">List a Property</h1>
-        {/*---PROPERTY-TYPE---*/}
-        <label className="form-label">Property Type</label>
-        <select name="property-type">
-          <option value="house">House</option>
-          <option value="land">Land</option>
-          <option value="unit/apartment">Unit/ Apartment</option>
-          <option value="acerage">Acerage</option>
-          <option value="townhouse">Townhouse</option>
-          <option value="rural">Rural</option>
-          <option value="villa">Villa</option>
-        </select>
-        {/*---ADDRESS---*/}
-        <label className="form-label">Address</label>
-        <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          autoComplete="street-address"
-        />
-        {/*---Description---*/}
-        <label className="form-label">Description</label>
-        <textarea
-          type="text-field"
-          name="description"
-          placeholder="Description"
-        />
-        {/*---Price---*/}
-        <label className="form-label">Price</label>
-        <input type="number" name="price" placeholder="$" />
-        {/*---LAND-SIZE---*/}
-        <label className="form-label">Land Size</label>
-        <input type="number" name="land-size" placeholder="Land Size (m2)" />
-        {/*---INSPECTION-TIME---*/}
-        <label className="form-label">Inspection Time</label>
-        <div className="inspection-time">
-          <input type="date" name="inspection-date" placeholder="Date" />
-          <input type="time" name="inspection-time" placeholder="Time" />
-        </div>
-        {/*---AMENITIES---*/}
-        <label className="form-label">Amenities</label>
-        <div className="amenities">
-          <div className="amenity">
-            <h3 className="amenity-label">Bedrooms</h3>
-            <input type="number" name="bedrooms" defaultValue="0" />
-          </div>
-          <div className="amenity">
-            <h3 className="amenity-label">Bathrooms</h3>
-            <input type="number" name="bathrooms" defaultValue="0" />
-          </div>
-          <div className="amenity">
-            <h3 className="amenity-label">Car Spaces</h3>
-            <input type="number" name="car-spaces" defaultValue="0" />
-          </div>
-        </div>
-        {/*----------------------SHARE------------------------*/}
-        <h1 className="listing-title">Share</h1>
-        <p className="listing-description">
-          This will post your listing onto the social media platforms you have
-          selected
-        </p>
-        <div className="media-boxes">
-          {/*Render linked in*/}
-          {props.stateCopy.linkedInConnected ? (
-            <MediaBox
-              handleToggle={props.handleToggle}
-              mediaLogo="linkedin"
-              mediaTitle="LinkedIn"
-              isChecked={toggleSettings.linkedIn}
+    return (
+      <div>
+        {/*---------------------FORM--------------------------*/}
+        <form onSubmit={this.handleSubmit} className="listing-media-form">
+          <h1 className="listing-title">List a Property</h1>
+          {/*---PROPERTY-TYPE---*/}
+          <label className="form-label">Property Type</label>
+          <select name="propertyType" onChange={this.handleChange}>
+            <option value="house">House</option>
+            <option value="land">Land</option>
+            <option value="unit/apartment">Unit/ Apartment</option>
+            <option value="acerage">Acerage</option>
+            <option value="townhouse">Townhouse</option>
+            <option value="rural">Rural</option>
+            <option value="villa">Villa</option>
+          </select>
+          {/*---ADDRESS---*/}
+          <label className="form-label">Address</label>
+          <input
+            type="text"
+            name="address"
+            placeholder="Address"
+            autoComplete="street-address"
+            onChange={this.handleChange}
+          />
+          {/*---Description---*/}
+          <label className="form-label">Description</label>
+          <textarea
+            type="text-field"
+            name="description"
+            placeholder="Description"
+            onChange={this.handleChange}
+          />
+          {/*---Price---*/}
+          <label className="form-label">Price</label>
+          <input
+            type="number"
+            name="price"
+            placeholder="$"
+            onChange={this.handleChange}
+          />
+
+          {/*---LAND-SIZE---*/}
+          <label className="form-label">Land Size</label>
+          <input
+            type="number"
+            name="landSize"
+            placeholder="Land Size (m2)"
+            onChange={this.handleChange}
+          />
+          {/*---INSPECTION-TIME---*/}
+          <label className="form-label">Inspection Time</label>
+          <div className="inspection-time">
+            <input
+              type="date"
+              name="inspectionDate"
+              placeholder="Date"
+              onChange={this.handleChange}
             />
-          ) : (
-            ''
-          )}
-          {/*Render Twitter*/}
-          {props.stateCopy.twitterConnected ? (
-            <MediaBox
-              handleToggle={props.handleToggle}
-              mediaLogo="twitter"
-              mediaTitle="Twitter"
-              isChecked={toggleSettings.twitter}
+            <input
+              type="time"
+              name="inspectionTime"
+              placeholder="Time"
+              onChange={this.handleChange}
             />
-          ) : (
-            ''
-          )}
-          <button className="upload-listing-button" type="submit">
-            Upload Listing
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+          </div>
+          {/*---AMENITIES---*/}
+          <label className="form-label">Amenities</label>
+          <div className="amenities">
+            <div className="amenity">
+              <h3 className="amenity-label">Bedrooms</h3>
+              <input
+                type="number"
+                name="bedrooms"
+                defaultValue="0"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="amenity">
+              <h3 className="amenity-label">Bathrooms</h3>
+              <input
+                type="number"
+                name="bathrooms"
+                defaultValue="0"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="amenity">
+              <h3 className="amenity-label">Car Spaces</h3>
+              <input
+                type="number"
+                name="carSpaces"
+                defaultValue="0"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          {/*----------------------SHARE------------------------*/}
+          <h1 className="listing-title">Share</h1>
+          <p className="listing-description">
+            This will post your listing onto the social media platforms you have
+            selected
+          </p>
+          <div className="media-boxes">
+            {/*Render linked in*/}
+            {this.props.stateCopy.linkedInConnected ? (
+              <MediaBox
+                handleToggle={this.props.handleToggle}
+                mediaLogo="linkedin"
+                mediaTitle="LinkedIn"
+                isChecked={this.props.stateCopy.linkedInToggleStatus}
+              />
+            ) : (
+              ''
+            )}
+            {/*Render Twitter*/}
+            {this.props.stateCopy.twitterConnected ? (
+              <MediaBox
+                handleToggle={this.props.handleToggle}
+                mediaLogo="twitter"
+                mediaTitle="Twitter"
+                isChecked={this.props.stateCopy.twitterToggleStatus}
+              />
+            ) : (
+              ''
+            )}
+            <button className="upload-listing-button" type="submit">
+              Upload Listing
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ListingsPage;
