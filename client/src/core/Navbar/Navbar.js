4b276c37674d34ee63a63bd750191ff8263944f4
Franky <27916006+fycdev@users.noreply.github.com>
2018-07-25 17:12:49 +1000

    refactor navbar

--- client/src/core/Navbar/Navbar.js
+++ client/src/core/Navbar/Navbar.js
@@ -1,57 +1,141 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
 
-const Navbar = (props) => {
-  const logoutLink = (
-    <Link
-      onClick={props.handleLogout}
-      className="navlink"
-      to="/"
-      style={{ textDecoration: 'none', color: '#908F8F' }}
-    >
-      Logout
-    </Link>
-  );
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: false,
+    };
+  }
 
-  const loginLink = (
-    <Link
-      className="navlink"
-      to="/"
-      style={{ textDecoration: 'none', color: '#908F8F' }}
-    >
-      Login
-    </Link>
-  );
-  const settingsLink = (
-    <Link
-      className="navlink"
-      to="/settings"
-      style={{ textDecoration: 'none', color: '#908F8F' }}
-    >
-      Settings
-    </Link>
-  );
-  const listingLink = (
-    <Link
-      className="navlink"
-      to="/listing"
-      style={{ textDecoration: 'none', color: '#908F8F' }}
-    >
-      Listing
-    </Link>
-  );
+  // Handle the logout by calling the logout API.
+  // If successful, set the success state to true to cause a rerender into a redirect.
+  // Then set success to false to prevent a second redirect.
+  handleLogout = async () => {
+    try {
+      await this.props.handleLogout();
+      // this.setState({ success: true });
+    } finally {
+      // this.setState({ success: false });
+    }
+  };
 
-  const loggedInLinks = (
-    <div>
-      {logoutLink}
-      {settingsLink}
-      {listingLink}
-    </div>
-  );
+  // Display the Home, Listing, Settings, and Logout links if the user is authenticated.
+  navbarLinks = () => {
+    let navbarLinks;
+    if (this.props.loggedIn) {
+      navbarLinks = (
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/listing"> Listing </Link>
+          </li>
+          <li>
+            <Link to="/settings"> Settings </Link>
+          </li>
+          <li>
+            <button onClick={this.handleLogout}>Logout</button>
+          </li>
+        </ul>
+      );
+    }
+    return navbarLinks;
+  };
 
-  return (
-    <div className="navbar">{props.loggedIn ? loggedInLinks : loginLink}</div>
-  );
+  // Redirect if the success (logout) is true.
+  render() {
+    // return this.state.success ? (
+    //   <Redirect to="/" />
+    // ) : (
+    //   <header>
+    //     <nav>
+    //       <h2> Realista </h2>
+    //       {this.navbarLinks()}
+    //     </nav>
+    //     {this.props.loggedIn && <p>Logged In</p>}
+    //   </header>
+    // );
+    return (
+      <header>
+        <nav>
+          <h2> Realista </h2>
+          {this.navbarLinks()}
+        </nav>
+        {this.props.loggedIn && <p>Logged In</p>}
+      </header>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  handleLogout: PropTypes.func,
+  loggedIn: PropTypes.bool,
+};
+
+Navbar.defaultProps = {
+  handleLogout: null,
+  loggedIn: null,
 };
 
 export default Navbar;
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const Navbar = (props) => {
+//   const logoutLink = (
+//     <Link
+//       onClick={props.handleLogout}
+//       className="navlink"
+//       to="/"
+//       style={{ textDecoration: 'none', color: '#908F8F' }}
+//     >
+//       Logout
+//     </Link>
+//   );
+
+//   const loginLink = (
+//     <Link
+//       className="navlink"
+//       to="/"
+//       style={{ textDecoration: 'none', color: '#908F8F' }}
+//     >
+//       Login
+//     </Link>
+//   );
+//   const settingsLink = (
+//     <Link
+//       className="navlink"
+//       to="/settings"
+//       style={{ textDecoration: 'none', color: '#908F8F' }}
+//     >
+//       Settings
+//     </Link>
+//   );
+//   const listingLink = (
+//     <Link
+//       className="navlink"
+//       to="/listing"
+//       style={{ textDecoration: 'none', color: '#908F8F' }}
+//     >
+//       Listing
+//     </Link>
+//   );
+
+//   const loggedInLinks = (
+//     <div>
+//       {logoutLink}
+//       {settingsLink}
+//       {listingLink}
+//     </div>
+//   );
+
+//   return (
+//     <div className="navbar">{props.loggedIn ? loggedInLinks : loginLink}</div>
+//   );
+// };
+
+// export default Navbar;
