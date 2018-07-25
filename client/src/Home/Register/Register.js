1a3de5958b096bd78ae78bd6a4775bcf1e620b8d
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-25 15:49:31 +1000

    add register button

--- client/src/Home/Register/Register.js
+++ client/src/Home/Register/Register.js
@@ -2,19 +2,35 @@ import React from 'react';
 import './register.css';
 
 class Register extends React.Component {
-  state = {
-    nameField: '',
-    companyField: '',
-    phoneField: ''
-  };
+  constructor() {
+    super();
+    this.state = {
+      signUp: false,
+      nameField: '',
+      companyField: '',
+      phoneField: ''
+    };
+  }
+
+  // handleRegister = () => {
+  //   this.setState()
+  //   }
 
   render() {
     return (
-      <div className="form-input">
-        <input type="name" placeholder="name" name="name" />
-        <input type="company" placeholder="company" name="company" />
-        <input type="phone" placeholder="phone" name="phone" />
-      </div>
+      <React.Fragment>
+        <div className="form-input">
+          <input type="name" placeholder="name" name="name" />
+          <input type="text" placeholder="email" name="emailField" />
+          <input type="password" placeholder="password" name="passwordField" />
+          <input type="company" placeholder="company" name="company" />
+          <input type="phone" placeholder="phone" name="phone" />
+        </div>
+        <button className="register" onClick={this.handleRegister}>
+          {this.state.signUp ? '' : 'Register'}
+        </button>
+        {this.state.error && <p>Oops! Something went wrong!</p>}
+      </React.Fragment>
     );
   }
 }
