8a53472e700d855baf533193ba735268192be24f
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-25 10:43:27 +1000

    add nameField, companyField, phoneField

--- client/src/Home/Register/Register.js
+++ client/src/Home/Register/Register.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Register() {
-  return <h1> Register </h1>;
+class Register extends React.Component {
+  state = {
+    nameField: '',
+    companyField: '',
+    phoneField: ''
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <input type="name" placeholder="name" name="name" />
+        <input type="company" placeholder="company" name="company" />
+        <input type="phone" placeholder="phone" name="phone" />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Register;
